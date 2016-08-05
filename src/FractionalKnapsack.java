5ebeae2a39566069264cf9afbbd5fefc4a6b9d98
Natu Lauchande <nlauchande@gmail.com>
2016-08-05 21:06:15 +0200

    Different algorithm selection

--- src/FractionalKnapsack.java
+++ src/FractionalKnapsack.java
@@ -1,14 +1,66 @@
-import java.util.Scanner;
+import java.util.*;
 
 /*
-#greedy
-Status : TODO
+#greedy #treemap
+Status : DONE
 */
 
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double value = 0;
         //write your code here
+
+        TreeMap<Integer,Double> hashMap = new TreeMap();
+
+        for (int i = 0; i < values.length; i++) hashMap.put(i,(double) values[i]/weights[i]) ;
+
+        SortedMap<Integer,Double> sortedMap = new TreeMap(
+                new Comparator<Integer>()
+                {
+                    @Override
+                    public int compare(Integer entryKey1,Integer entryKey2)
+                    {
+
+                        if ((hashMap.get(entryKey1) > hashMap.get(entryKey2)))
+                        {
+                            return -1;
+                        }
+                        else if ( (hashMap.get(entryKey1) < hashMap.get(entryKey2) ))
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+
+                    }
+                }
+        );
+
+        sortedMap.putAll(hashMap);
+
+        int capacityLeft = capacity;
+        int index = 0;
+
+        while (capacityLeft>0 && !sortedMap.isEmpty())
+        {
+            index = sortedMap.firstKey();
+
+            if (weights[index] <= capacityLeft)
+            {
+                capacityLeft -= weights[index];
+                value += values[index];
+            }
+            else
+            {
+                value += ( (double) capacityLeft/weights[index]) * ( values[index]);
+                capacityLeft = 0;
+            }
+            sortedMap.remove(index);
+
+        }
+
 
         return value;
     }
